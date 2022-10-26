1f1788c805fd21d70fc41b59d569faaf1135fab4
Utkarsh Saxena <little@Utkarsh-Saxena.local>
2022-10-26 22:32:18 +0530

    Nodejs Authentication Completed

---
+++ config/key.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MongoURI:
+    "mongodb://localhost:27017/?serverSelectionTimeoutMS=5000&connectTimeoutMS=10000&3t.uriVersion=3&3t.connection.name=todo-list&3t.alwaysShowAuthDB=true&3t.alwaysShowDBFromUserRole=true",
+};
